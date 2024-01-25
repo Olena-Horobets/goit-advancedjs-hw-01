438a0d128f03b1413fa448a02923113ae9cb1c95
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2024-01-25 18:01:17 -0330

    player timer updated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY = 'videoplayer-current-time';
+let { seconds } = JSON.parse(localStorage.getItem(KEY)) ?? { seconds: 0 };
+
+const onTimeUpdate = ({ duration, percent, seconds }) => {
+  localStorage.setItem(KEY, JSON.stringify({ duration, percent, seconds }));
+};
+const throttledFn = throttle(onTimeUpdate, 1000);
+
+player
+  .setCurrentTime(seconds)
+  .then()
+  .catch(function (err) {
+    console.log(err);
+  });
+player.on('timeupdate', throttledFn);
