c307ea3222db6066c72d8166901f495284d95dd6
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2024-01-28 12:51:02 -0330

    form functional added

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const KEY = 'feedback-form-state';
+const data = JSON.parse(localStorage.getItem(KEY)) ?? {
+  email: '',
+  message: '',
+};
+
+const setFormData = (form, data) => {
+  form.elements.email.value = data.email;
+  form.elements.message.value = data.message;
+};
+
+const onInput = throttle(e => {
+  data[e.target.name] = e.target.value;
+  localStorage.setItem(KEY, JSON.stringify(data));
+}, 500);
+
+const onSubmit = e => {
+  e.preventDefault();
+
+  console.log(data);
+  localStorage.removeItem(KEY);
+  setFormData(form, { email: '', message: '' });
+};
+
+setFormData(form, data);
+form.addEventListener('submit', onSubmit);
+form.addEventListener('input', onInput);
